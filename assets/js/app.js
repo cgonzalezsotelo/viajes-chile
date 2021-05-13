10bb8e4e6199a79731e861fbd3b1bd54d9506cf6
Sotelo <cgonzalezsotelo@gmail.com>
2021-05-13 12:02:29 -0400

    arreglos visuales generales y agregando funcionalidad para cambiar color de fondo al nav cuando se hace scroll

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,16 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(document).ready(function(){
+  $(window).scroll(function(){
+  	var scroll = $(window).scrollTop();
+	  if (scroll > 300) {
+	    $(".nav-color").css("background" , "rgb(12, 218, 218)");
+	  }
+
+	  else{
+		  $(".nav-color").css("background" , "rgb(250,250,250,0)");  	
+	  }
+  })
+})
