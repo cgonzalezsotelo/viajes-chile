94b47a983604322a311ee95edd8db475c31b2c8e
Sotelo <cgonzalezsotelo@gmail.com>
2021-05-13 13:14:13 -0400

    arreglos detalles en html y incorporacion de tooltips

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,18 @@
-// habilitación tooltips bootstrao 4.6
+// habilitación tooltips bootstrap 4.6
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
+// Cambio de color nav al hacer scroll
 $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 300) {
-	    $(".nav-color").css("background" , "rgb(12, 218, 218)");
+	    $(".nav-color-scroll").css("background" , "rgb(12, 218, 218)");
 	  }
 
 	  else{
-		  $(".nav-color").css("background" , "rgb(250,250,250,0)");  	
+		  $(".nav-color-scroll").css("background" , "rgb(250,250,250,0)");  	
 	  }
   })
 })
