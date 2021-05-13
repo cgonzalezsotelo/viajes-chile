10e268869c04929d34d3d91ddc4d2058931d436e
Sotelo <cgonzalezsotelo@gmail.com>
2021-05-13 14:49:50 -0400

    cambio de smooth scroll css por jQuery

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,20 @@
 // habilitación tooltips bootstrap 4.6
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
+  
+  $("a").click(function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+
+      var gato = this.hash;
+
+      $('html, body').animate({
+        scrollTop: $(gato).offset().top
+      }, 800, function() {
+        window.location.hash = gato;
+      });
+    }
+  });
 })
 
 // Cambio de color nav al hacer scroll
